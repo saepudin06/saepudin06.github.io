7559b253a16db69ac0aee8576f00839d10118e69
saepudin06 <saepudin@vsi.co.id>
2021-01-23 23:00:24 +0700

    update

--- data/token.js
+++
@@ -1,6 +0,0 @@
-var daftarHarga = '[ '+
-                  '{ "harga" : 20000, "jual" : 20000, "untung": 500 },'+
-                  '{ "harga" : 50000, "jual" : 50000, "untung": 500 },'+
-                  '{ "harga" : 100000, "jual" : 100000, "untung": 1000 },'+                  
-                  '{ "harga" : 200000, "jual" : 200000, "untung": 1000 }'+                  
-                  ']';
